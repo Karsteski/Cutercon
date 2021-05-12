9dae3ca7cc31c9835298c49d991680f6463580a5
Kareem Skinner <kareem@karski.io>
2021-05-12 14:26:39 -0400

    Initial Qt setup

--- cutercon.py
+++ cutercon.py
@@ -1,3 +1,40 @@
-x = 69
+import sys
 
-print(69)
+from PyQt6.QtWidgets import QApplication, QVBoxLayout, QWidget, QLineEdit, QTextEdit
+from PyQt6.QtGui import QIcon
+
+# Temp globals for testing...
+HOST = "192.168.0.10"
+PASSWORD = "password"
+PORT = 25575
+
+
+class CuterconQt(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Cutercon")
+        self.setWindowIcon(QIcon("./minecraft-logo.ico"))
+        self.resize(500, 500)  # width, height
+
+        layout = QVBoxLayout()
+        self.setLayout(layout)
+
+        self.outputField = QTextEdit()
+        self.inputField = QLineEdit(returnPressed=self.mirrorText)
+
+        layout.addWidget(self.outputField)
+        layout.addWidget(self.inputField)
+
+    def mirrorText(self):
+        inputText = self.inputField.text()
+        self.outputField.setText("Output: {0}".format(inputText))
+
+
+if __name__ == "__main__":
+    # Perhaps wrap below in Run() and Exit()
+    app = QApplication([])
+
+    window = CuterconQt()
+    window.show()
+
+    sys.exit(app.exec())
