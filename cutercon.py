e935a22a81f8ba7c602d7189200786d582f6d9ec
Kareem Skinner <kareem@karski.io>
2021-05-12 17:21:20 -0400

    Finished PacketType(IntEnum) and connect()

--- cutercon.py
+++ cutercon.py
@@ -1,3 +1,5 @@
+from enum import IntEnum
+import socket
 import sys
 
 from PyQt6.QtWidgets import QApplication, QVBoxLayout, QWidget, QLineEdit, QTextEdit
@@ -7,6 +9,65 @@ from PyQt6.QtGui import QIcon
 HOST = "192.168.0.10"
 PASSWORD = "password"
 PORT = 25575
+
+
+class PacketType(IntEnum):
+    """
+    Packet type field is a 32-bit little endian integer,
+    indicating the purpose of the packet.
+    The value repetition is not an error.
+    """
+    SERVERDATA_RESPONSE_VALUE = 0
+    SERVERDATA_EXECCOMMAND = 2
+    SERVERDATA_AUTH_RESPONSE = 2
+    SERVERDATA_AUTH = 3
+
+
+class CuterconException(Exception):
+    pass
+
+
+class Cutercon(object):
+    """
+    Remote Console Client for Minecraft: Java Edition
+    """
+
+    cutercon_socket = None
+
+    def __init__(self, host, password, port=25575):
+        self.host = host
+        self.password = password
+        self.port = port
+
+    def __enter__(self):
+        self.connect()
+        return self
+
+    # Guarantees socket is closed upon program exit.
+    def __exit__(self, exceptionType, exceptionValue, exceptionTraceback):
+        self.disconnect()
+
+    def connect(self):
+        # Create new socket w/ the address & protocol family (AF_INET is IPv4).
+        # SOCK_STREAM is a type of socket known as a stream socket,
+        # providing two-way communication between client and server.
+        self.cutercon_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.connect((self.host, self.port))
+        self._sendData(PacketType.SERVERDATA_AUTH, self.password)
+
+    def disconnect(self):
+
+
+    def _readData(self, length):
+        
+    
+    def _sendData(self, outputType, outputData):
+
+    
+    def command(self, command):
+
+    
+
 
 
 class CuterconQt(QWidget):
