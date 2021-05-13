19ea7afb40ba6c86b7db02e6bb9734f316789d61
Kareem Skinner <kareem@karski.io>
2021-05-13 11:48:44 -0400

    Finished networking code

--- cutercon.py
+++ cutercon.py
@@ -1,5 +1,7 @@
 from enum import IntEnum
 import socket
+import select
+import struct
 import sys
 
 from PyQt6.QtWidgets import QApplication, QVBoxLayout, QWidget, QLineEdit, QTextEdit
@@ -16,6 +18,7 @@ class PacketType(IntEnum):
     Packet type field is a 32-bit little endian integer,
     indicating the purpose of the packet.
     """
+
     # Incoming payload is the output of the command. Commands can return nothing.
     CommandResponse = 0
 
@@ -38,7 +41,7 @@ class Cutercon(object):
     Remote Console Client for Minecraft: Java Edition
     """
 
-    cutercon_socket = None
+    cuterconSocket = None
 
     def __init__(self, host, password, port=25575):
         self.host = host
@@ -54,17 +57,24 @@ class Cutercon(object):
         self.disconnect()
 
     def connect(self):
-        # Create new socket w/ the address & protocol family (AF_INET is IPv4).
-        # SOCK_STREAM is a type of socket known as a stream socket,
-        # providing two-way communication between client and server.
-        self.cutercon_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        """
+        Create new socket w/ the address & protocol family (AF_INET is cute).
+        SOCK_STREAM is a type of socket known as a stream socket,
+        providing two-way communication between client and server.
+
+        Call disconnect() after finishing with the socket!
+        """
+        self.cuterconSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.connect((self.host, self.port))
         self._sendData(PacketType.Login, self.password)
 
     def disconnect(self):
-        if self.cutercon_socket is not None:
+        """
+        Always close socket after finishing!
+        """
+        if self.cuterconSocket is not None:
             self.socket.close()
-            self.cutercon_socket = None
+            self.cuterconSocket = None
 
     def _readData(self, length):
         data = b""
@@ -74,9 +84,66 @@ class Cutercon(object):
             data += self.socket.recv(length - len(data))
         return data
 
-    def _sendData(self, outputType, outputData):
+    def _sendData(self, outgoingPacketType, outgoingPacketData):
+        if self.socket is None:
+            raise CuterconException("A socket must be connected before sending data.")
+
+        INITIAL_BYTE = 0
+        EMPTY_TWO_BYTE_STRING = b"\x00\x00"
+
+        # Must first send a request packet.
+        outgoingPayload = (
+            struct.pack("<ii", INITIAL_BYTE, outgoingPacketType)
+            + outgoingPacketData.encode("utf8")
+            + EMPTY_TWO_BYTE_STRING
+        )
+
+        outgoingPayloadLength = struct.pack("<i", len(outgoingPayload))
+        self.cuterconSocket.send(outgoingPayloadLength + outgoingPayload)
+
+        # Now read response packets.
+        incomingData = ""  # Must be defined outside loop for concatenation
+
+        while True:
+            # First, read a packet
+            BYTES_IN_32BIT_INT = 4
+            BYTES_IN_ID_AND_TYPE_FIELD = 8
+
+            # Note that unpack returns a tuple even for one item.
+            # The empty items are "thrown" away.
+            (incomingPayloadLength,) = struct.unpack(
+                "<i", self._readData(BYTES_IN_32BIT_INT)
+            )
+            incomingPayload = self._readData(incomingPayloadLength)
+            incomingPayloadID, incomingPayloadType = struct.unpack(
+                "<ii", incomingPayload[:BYTES_IN_ID_AND_TYPE_FIELD]
+            )
+            incomingDataPartial, incomingPayloadPadding = (
+                incomingPayload[BYTES_IN_ID_AND_TYPE_FIELD:-2],
+            )
+            incomingPayload[-2:]
+
+            # Sanity checking
+            if incomingPayloadPadding != b"\x00\x00":
+                raise CuterconException("Incorrect packet padding!")
+
+            WRONG_PASSWORD_ID = -1
+            if incomingPayloadID == WRONG_PASSWORD_ID:
+                raise CuterconException("Authentication failed: Wrong password!")
+
+            # Store response
+
+            incomingData += incomingDataPartial.decode("utf8")
+
+            # Return response if no more data to receive
+
+            TIMEOUT = 0.0  # A timeout value of zero specifies a poll and never blocks.
+            if len(select.select([self.cuterconSocket], [], [], TIMEOUT)[0]) == 0:
+                return incomingData
 
     def command(self, command):
+        result = self._sendData(PacketType.Command, command)
+        return result
 
 
 class CuterconQt(QWidget):
